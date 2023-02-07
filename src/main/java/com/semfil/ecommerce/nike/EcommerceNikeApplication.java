3c772dcc4c9e5b3d2c90e5393b8551a292df2848
Sebastian Valbuena <sebastianvalbuena25@gmail.com>
2023-02-07 17:33:47 -0500

    Initiating the project

---
+++ src/main/java/com/semfil/ecommerce/nike/EcommerceNikeApplication.java
@@ -0,0 +1,13 @@
+package com.semfil.ecommerce.nike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceNikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceNikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semfil/ecommerce/nike/EcommerceNikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semfil.ecommerce.nike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceNikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
